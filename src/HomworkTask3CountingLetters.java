fdccbaf2ee3e58308f9fe3359bd945f02434c133
Nedjo <nedjmi.ibryamov@gmail.com>
2023-06-04 18:40:48 +0300

    Homework task 3 is done

---
+++ src/HomworkTask3CountingLetters.java
@@ -0,0 +1,20 @@
+public class HomworkTask3CountingLetters {
+    public static void main(String[] args) {
+        System.out.println(countingYAndZLettersAtTheEndOfTheWords("fez day"));
+        System.out.println(countingYAndZLettersAtTheEndOfTheWords("day fyyyz"));
+        System.out.println(countingYAndZLettersAtTheEndOfTheWords("Dad fez"));
+        System.out.println(countingYAndZLettersAtTheEndOfTheWords("Yellowy fez day"));
+    }
+
+    public static int countingYAndZLettersAtTheEndOfTheWords(String text) {
+        int countOfYAndZ = 0;
+        for (String word : text.split(" ")) {
+            char lastLetter = word.charAt(word.length() - 1);
+            if(lastLetter == 'y' || lastLetter == 'z') {
+                countOfYAndZ++;
+            }
+        }
+
+        return countOfYAndZ;
+    }
+}
